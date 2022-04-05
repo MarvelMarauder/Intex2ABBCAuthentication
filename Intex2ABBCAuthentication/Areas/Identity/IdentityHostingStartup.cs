8742f933142e2337b05f5e43086169860ec4ab8f
MarvelMarauder <90361775+MarvelMarauder@users.noreply.github.com>
2022-04-04 21:22:07 -0600

    Add project files.

---
+++ Intex2ABBCAuthentication/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Intex2ABBCAuthentication.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Intex2ABBCAuthentication.Areas.Identity.IdentityHostingStartup))]
+namespace Intex2ABBCAuthentication.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
