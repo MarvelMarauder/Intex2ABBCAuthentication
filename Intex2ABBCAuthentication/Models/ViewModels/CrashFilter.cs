5ebc3451a7d5303611073af79d1e7230b25e4d56
MarvelMarauder <90361775+MarvelMarauder@users.noreply.github.com>
2022-04-05 17:15:41 -0600

    almost done with filter

---
+++ Intex2ABBCAuthentication/Models/ViewModels/CrashFilter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Intex2ABBCAuthentication.Models.ViewModels
+{
+    public class CrashFilter
+    {
+        public string county { get; set; }
+        public string city { get; set; }
+        public double severity { get; set; }
+        public int year { get; set; }
+        public int month { get; set; }
+    }
+}
