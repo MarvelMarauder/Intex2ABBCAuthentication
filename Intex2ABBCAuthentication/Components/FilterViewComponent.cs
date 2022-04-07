74658a234d503020da2629d678af168a011a0dc1
MarvelMarauder <90361775+MarvelMarauder@users.noreply.github.com>
2022-04-06 22:46:11 -0600

    recent

---
+++ Intex2ABBCAuthentication/Components/FilterViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Intex2ABBCAuthentication.Components
+{
+    public class FilterViewComponent : ViewComponent
+    {
+        public FilterViewComponent()
+        {
+        }
+        public async  Task<IViewComponentResult> InvokeAsyn()
+        {
+            return View();
+        }
+    }
+}
