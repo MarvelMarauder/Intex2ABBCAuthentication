45a81e17d0b59873e8d7bbf1e5021d06cf596013
MarvelMarauder <90361775+MarvelMarauder@users.noreply.github.com>
2022-04-06 20:18:08 -0600

    CRUD IS DONE

--- Intex2ABBCAuthentication/Models/EFCrashRepository.cs
+++ Intex2ABBCAuthentication/Models/EFCrashRepository.cs
@@ -17,6 +17,7 @@ namespace Intex2ABBCAuthentication.Models
 
         public void SaveCrash(CarCrash c)
         {
+            context.Update(c);
             context.SaveChanges();
         }
         public void CreateCrash(CarCrash c)
